a60e50173cd0d7435a8d959ebb942ce4413bae9a
zap-rowsdower <brock.whitbread@gmail.com>
2018-10-05 10:52:38 -0700

    update `mongoose.connect` to remove console warnings

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useMongoClient: true});
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", { useNewUrlParser: true } );
 
 module.exports.Bonsai = require('./bonsai.js');
 
